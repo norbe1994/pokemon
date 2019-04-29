73db97bec2d3eda10fa7b888bf394ca398f8901f
Norberto Caceres <norbe1994@yahoo.com>
2019-04-29 10:15:12 -0500

    substituir imagen de pokecard por una de mas calidad

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import './Pokecard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+const padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number)
 
 class Pokecard extends Component {
   render() {
-    const imgSrc = `${POKE_API}${this.props.id}.png`
+    const imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
