cc96228b0740c10909018fee79bea3be1ebed7dc
Norberto Caceres <norbe1994@yahoo.com>
2019-04-29 11:13:00 -0500

    improve styling

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -3,27 +3,18 @@ import './Pokedex.css'
 import Pokecard from './Pokecard'
 
 class Pokedex extends Component {
-  static defaultProps = {
-    pokemon: [
-      { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
-      { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
-      { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
-      { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
-      { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
-      { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
-      { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
-      { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
-    ]
-  }
-
   render() {
-    let handResult = this.props.isWinner ? 'WINNER!' : 'LOSER!'
-    if (this.props.isTie) handResult = "IT'S A TIE!"
+    let handResult
+    if (this.props.isWinner) {
+      handResult = <h1 className="Pokedex-winner">Winner!</h1>
+    } else {
+      handResult = <h1 className="Pokedex-loser">Loser!</h1>
+    }
+    if (this.props.isTie) handResult = <h1 className="Pokedex-tie">It's a tie!</h1>
     return (
       <div className="Pokedex">
-        <h1>Pokedex!</h1>
-        <p>Total Experience: {this.props.exp}</p>
-        <p>{handResult}</p>
+        {handResult}
+        <h4>Total Experience: {this.props.exp}</h4>
         <div className="Pokedex-cards">
           {this.props.pokemon.map(p => (
             <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} key={p.id} />
